92e8807cf186a48e02561ac9bf046a29b05dcd8c
Mihir Kumar <mihirpandey.13@gmail.com>
2021-06-20 16:14:11 +0530

    Message fallback made better

--- Course Day 9/ASSIGNMENT-crud-node-express-jwt/src/middleware/users.js
+++ Course Day 9/ASSIGNMENT-crud-node-express-jwt/src/middleware/users.js
@@ -5,7 +5,7 @@ exports.validateUser = (req, res, next) => {
         jwt.verify(req.headers.authorization.split(' ')[1], req.app.get('secretKey'), (err, decoded) => {
             if (err) {
                 res.locals.status = 401;
-                res.locals.error = err.message;
+                res.locals.error = err.message || 'TOKEN NOT VALID!';
                 next();
             } else {
                 // add user id to request
